3272af3ae18386329c2499eb8c26c7602b93325e
stanislawchociej <staschociej2@gmail.com>
2025-05-14 15:13:28 +0200

    Update cart.py
    Dodanie koszyka

---
+++ zabka/cart.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
+from zabka.Frog import get_products
+
+def dodaj_do_koszyka(tree, koszyk_listbox):
+    selected_item = tree.focus()
+    if not selected_item:
+        messagebox.showwarning("Brak wyboru", "Wybierz produkt z listy.")
+        return
+
+    item_values = tree.item(selected_item, "values")
+    produkt = f"{item_values[0]} - {item_values[1]} zł"
+    koszyk_listbox.insert(tk.END, produkt)

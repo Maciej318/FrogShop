c68255d2791000efaca179903d72b9703e156c82
lybsoon <kacper.breczko@wp.pl>
2025-05-22 16:43:41 +0200

    Komentarz

--- zabka/login.py
+++ zabka/login.py
@@ -2,6 +2,7 @@ from session import *
 from tkinter import messagebox
 from zabka.Frog import get_customers
 
+
 # Logowanie uzytkownika
 def client_login(name_entry):
     username = name_entry.get()
@@ -25,7 +26,7 @@ def client_login(name_entry):
         return False
 
 
-
+# Logowanie jako admin
 def admin_login(name_entry):
     if name_entry.get().lower() == "admin":
         return True
@@ -33,9 +34,10 @@ def admin_login(name_entry):
         messagebox.showerror("Błąd", "Nie można zalogować jako admin")
         return False
 
-def spr():
-    if is_logged_in():
-        messagebox.showinfo("Status:", f"zalogowany jako: {get_current_user()}")
-    else:
-        messagebox.showinfo("Status:", "Nikt nie jest zalogowany")
-
+# Sprawdzenie aktualnego uzytkownika
+# def spr():
+#     if is_logged_in():
+#         messagebox.showinfo("Status:", f"zalogowany jako: {get_current_user()}")
+#     else:
+#         messagebox.showinfo("Status:", "Nikt nie jest zalogowany")
+#

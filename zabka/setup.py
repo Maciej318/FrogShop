d4fe2369c2b0ef99217d0f4eb7fe9488057d2dd1
lybsoon <kacper.breczko@wp.pl>
2025-05-27 22:10:45 +0200

    zmiana

--- zabka/setup.py
+++ zabka/setup.py
@@ -1,10 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 # Pakiet Frog
 setup(
     name="Frog",
     version="0.0.1",
-    packages=["Frog"],
+    packages=find_packages(where="zabka"),
+    package_dir={"": "zabka"},
     description="Frog package for customers and products",
     include_package_data=True,
     url="https://github.com/Maciej318/FrogShop.git",
